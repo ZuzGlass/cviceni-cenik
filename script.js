a97acfaf3c688ae0888727ffec260d65b73fe487
Zuzana Brýlová <zuzana.brylova@gmail.com>
2024-03-15 19:58:26 +0100

    Finální verze.

---
+++ script.js
@@ -0,0 +1,11 @@
+const selectPlan = (planNumber) => {
+    const zrusVyber = document.querySelector(".plan--selected")
+    zrusVyber.classList.remove("plan--selected")
+    
+    const vyber = document.querySelector(`#plan${planNumber}`)
+    vyber.classList.add("plan--selected")
+    }
+
+    const var1 = (selectPlan(1)) 
+    const var2 = (selectPlan(2))
+    const var3 = (selectPlan(3))
